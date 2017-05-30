82517dea4ee6f42ccfa24cde4b300f852a29a84b
Nikolaj Pogněrebko <nikolaj.pognerebko@alza.cz>
2017-05-30 14:00:52 +0200

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Nikolaj Pogněrebko on 30/05/2017.
+//  Copyright © 2017 NIkolaj Pogněrebko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
