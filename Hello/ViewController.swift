4a1a1cf94dc5ba5f3d826b8e2a08f39eac96fa30
Nikolaj Pogněrebko <jasin755@users.noreply.github.com>
2017-06-09 21:29:07 +0200

    Revert "Updated view controller"

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        //Test
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
